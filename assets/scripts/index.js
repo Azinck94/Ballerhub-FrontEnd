1ea52a07533a717f1337096c0fe69e9349c814a5
azinck94 <Andrew_zinck@student.uml.edu>
2017-07-22 16:44:19 -0400

    hero

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,9 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const getFormFields = require('../../lib/get-form-fields');
+const authEvents = require('./user-authorization/events');
+const playerEvents = require('./player-requests/events');
 
 $(() => {
   setAPIOrigin(location, config)
@@ -12,3 +15,25 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 require('./example')
+
+$(() => {
+    authEvents.addHandlers();
+    playerEvents.addHandlers();
+  $('#select-sign-up').on('click', function(){
+    $('#sign-up-modal').modal('show');
+  });
+  $('.sign-up-btn').on('click', function(){
+    $('#sign-up-modal').modal('hide');
+  });
+  $('#select-sign-in').on('click', function(){
+    $('#sign-in-modal').modal('show');
+  });
+  $('.sign-in-btn').on('click', function(){
+    $('#sign-in-modal').modal('hide');
+  });
+  $('#select-change-password').on('click', function(){
+    $('#change-password-modal').modal('show');
+  });
+  $('.change-password-btn').on('click', function(){
+    $('#change-password-modal').modal('hide');
+  });
